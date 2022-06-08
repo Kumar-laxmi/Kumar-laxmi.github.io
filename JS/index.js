425aeefeccec2d28f4c7ff162f1cca86b00935de
Kumar-laxmi <laxmikantk2002@gmail.com>
2022-06-08 11:38:33 +0530

    Animation effects added

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+import initScrollReveal from "./scrollReveal";
+import initTiltEffect from "./tiltAnimation";
+import { targetElements, defaultProps } from "./scrollRevealConfig";
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
